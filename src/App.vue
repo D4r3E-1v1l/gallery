56541741acae90bca89bc44f576dcc88d65c2f50
Shiyu Liu <minds.over.actions@gmail.com>
2022-12-12 19:07:07 -0500

    add sth

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,18 @@
 <template>
-  <Dog msg="Welcome to Your Vue.js Application"/>
+  <header>
+    <h1>
+      Different Stages of a Cute Dog
+    </h1>
+  </header>
+  <main>
+    <Dog/>
+  </main>
+  <footer>
+    <div>
+      <h3>Welcome to My Site</h3>
+      <p>© 2022. Shiyu Liu.  All Rights Reserved.</p>
+    </div>
+  </footer>
 </template>
 
 <script>
@@ -14,12 +27,52 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  margin: 0;
+  padding: 0;
+  -webkit-box-sizing: border-box;
+  box-sizing: border-box;
 }
+
+body {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  background-color: ghostwhite;
+}
+
+header {
+  background-color: #3b3b3b;
+}
+
+main {
+  height: 800px;
+}
+
+footer {
+  display: flex;
+  justify-content: center;
+  background-color: #3b3b3b;
+}
+
+footer div {
+  padding-top: 8px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  color: white;
+}
+
+footer div p {
+  padding-top: 24px;
+  padding-bottom: 10px;
+  font-size: small;
+}
+
+h1 {
+  color: ghostwhite;
+  padding-top: 20px;
+  padding-bottom: 20px;
+  display: flex;
+  justify-content: center;
+}
+
 </style>
--- src/components/Dog.vue
+++ src/components/Dog.vue
@@ -1,11 +1,26 @@
 <template>
   <div class="hello">
     <h2 id="stage">Stage 4</h2>
-    <img alt="dog"
-         src="@/assets/image4.png"
-         width="500"
-         height="600"
-         @mouseenter="hover"/>
+    <div id="dog-img">
+      <img alt="dog"
+           src="@/assets/image4.png"
+           width="360"
+           height="520"
+           @mouseenter="hover"/>
+    </div>
+
+    <div id="pet-name">
+      <p>
+        {{ text }}
+      </p>
+      <input value="suggestion"
+             @input="changeText"/>
+      <button @click="setText">send</button>
+      <p>
+        {{ thankyou }}
+      </p>
+    </div>
+
   </div>
 </template>
 
@@ -25,23 +40,44 @@ let imgList = [
 ]
 
 export default {
-  name: 'HelloWorld',
-  props: {
-
+  name: 'CuteDog',
+  data() {
+    return {
+      text: 'Name suggestion for my cutie pet',
+      thankyou: ''
+    }
   },
 
   methods: {
 
-    hover(elem) {
+    hover(e) {
       if (index <= 0) {
         return;
       }
+
       index--;
-      console.log(index);
-      elem.target.src = imgList[index % 4];
+      e.target.src = imgList[index];
       let stage = document.getElementById("stage");
       stage.innerText = "stage" + (index + 1);
 
+    },
+
+    changeText(e) {
+      this.text = e.target.value;
+    },
+
+    setText() {
+      let t = this.text;
+
+      console.log(t)
+
+      if (t === "") {
+        this.text = 'cannot send empty name.'
+        return
+      }
+
+
+      this.thankyou = 'Thanks for your suggestion!'
     }
   }
 }
@@ -49,18 +85,73 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+
+#stage {
+  padding-top: 20px;
+  padding-bottom: 40px;
+  display: flex;
+  justify-content: center;
+  color: black;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+#dog-img {
+  display: flex;
+  justify-content: center;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+#dog-img img {
+  border-radius: 25px;
+  box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+  object-fit: contain;
 }
-a {
-  color: #42b983;
+
+#pet-name {
+  padding-top: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
+
+#pet-name input {
+  width: 200px;
+  margin-bottom: 14px;
+  padding: 6px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+
+  -webkit-box-shadow: inset 0 1px 1px rgba(0,0,0,.075);
+  box-shadow: inset 0 1px 1px rgba(0,0,0,.075);
+  -webkit-transition: border-color ease-in-out .15s,-webkit-box-shadow ease-in-out .15s;
+  -o-transition: border-color ease-in-out .15s,box-shadow ease-in-out .15s;
+  transition: border-color ease-in-out .15s,box-shadow ease-in-out .15s;
+}
+
+#pet-name input:focus {
+  border-color: #ffe54c;
+  border-width: 1px;
+  outline: 0;
+  /*-webkit-box-shadow: none;*/
+  /*box-shadow: none;*/
+  -webkit-box-shadow: inset 0 1px 1px rgba(0, 0, 0, .075), 0 0 8px rgba(255, 214, 0, .7);
+  box-shadow: inset 0 1px 1px rgba(0, 0, 0, .075), 0 0 8px rgba(255, 214, 0, .7);
+}
+
+#pet-name p {
+  text-align: center;
+  padding-bottom: 14px;
+}
+
+#pet-name button {
+  padding: 0.6em 3em;
+  color: rgb(0, 0, 0);
+  background-color: #DDDDDDFF;
+  font-size: 1em;
+  font-weight: 600;
+  border-radius: 5px;
+  border-width: 0;
+}
+
+#pet-name button:hover {
+  background-color: #B0B0B0FF;
+}
+
 </style>
